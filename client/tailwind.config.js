d29b418e78e3e52e1403167da8e550bbf8bd99e6
Sithum Dilshan <shanukasithum28@gmail.com>
2023-05-21 18:06:23 +0530

    modify tailwind file

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
 
+export const content = ["./src/**/*.{js,jsx}"];
+export const theme = {
+  extend: {
+    screens: {
+      xs: "480px",
+    },
+    fontFamily: {
+      inter: ["Inter var", "sans-serif"],
+    },
+    boxShadow: {
+      card: "0 0 1px 0 rgba(189,192,207,0.06),0 10px 16px -1px rgba(189,192,207,0.2)",
+      cardhover:
+        "0 0 1px 0 rgba(189,192,207,0.06),0 10px 16px -1px rgba(189,192,207,0.4)",
+    },
+  },
+};
+export const plugins = [];
